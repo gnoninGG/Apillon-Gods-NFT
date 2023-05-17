fed7f57aed6f57bbf03569f48fe4c2e3466726d5
gnoninGG <116506146+gnoninGG@users.noreply.github.com>
2023-05-18 04:22:02 +0800

    Update env.js

--- js/env.js
+++ js/env.js
@@ -1,4 +1,4 @@
-const nftAddress = ""; // Paste the address of the NFT collection.
+const nftAddress = "0xb6c365Aa40e9F33c209CBF45381F53E37D8c9927"; // Paste the address of the NFT collection.
 // uncomment the chain you are using:
 const chainId = "0x507"; // Moonbase
-// const chainId = "0x504"; // Moonbeam
+
